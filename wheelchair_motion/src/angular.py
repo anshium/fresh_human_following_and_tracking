beb8c8ce0d654546ce0858813b5b1560b5f7e363
--local <--local>
2024-07-17 18:26:30 +0530

    Angular motion worksgit add .git add .

--- wheelchair_motion/src/angular.py
+++ wheelchair_motion/src/angular.py
@@ -2,13 +2,15 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from your_package.msg import BoundingBox  # Adjust your_package.msg based on your message definition
+from human_world_coordinates.msg import BoundingBox  # Adjust your_package.msg based on your message definition
 
 # Global variables for image dimensions (example)
 image_width = 1280
 image_height = 720
 
 def bounding_box_callback(msg):
+    global cmd_vel_pub
+
     x1 = msg.x1
     y1 = msg.y1
     x2 = msg.x2
@@ -29,19 +31,21 @@ def bounding_box_callback(msg):
     twist_msg = Twist()
     
     # Adjust linear and angular velocities based on distance
-    twist_msg.linear.x = 0.1  # Example linear velocity
-    twist_msg.angular.z = 0.01 * (bbox_center_x - image_midpoint_x)  # Example angular velocity
+    twist_msg.linear.x = 0.0  # Example linear velocity
+    twist_msg.angular.z = -0.001 * (bbox_center_x - image_midpoint_x)  # Example angular velocity
     
     cmd_vel_pub.publish(twist_msg)
 
 def main():
+    global cmd_vel_pub
     rospy.init_node('angular_movement')
     
+    # Create Twist publisher for cmd_vel topic
+    cmd_vel_pub = rospy.Publisher('wheelchair_diff/cmd_vel', Twist, queue_size=10)
+
     # Subscribe to BoundingBox topic
     rospy.Subscriber('/yolo/bounding_boxes', BoundingBox, bounding_box_callback)
     
-    # Create Twist publisher for cmd_vel topic
-    cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     
     rospy.spin()
 
